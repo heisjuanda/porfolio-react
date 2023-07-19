bf1e2c7b82625a7f84f630e8c2bea62af11a729b
Juan David Moreno Alfonso <jdma253@gmail.com>
2023-07-19 17:24:53 -0500

    heisjuanda => vite config

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,3 @@
-/*import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { terser } from 'rollup-plugin-terser';
-import cssnano from 'cssnano';
-import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
-
-export default defineConfig({
-  plugins: [
-    react(),
-    terser(), 
-    cssnano(),
-  ],
-  build: {
-    rollupOptions: {
-      output: {
-        inlineDynamicImports: true,
-        manualChunks: undefined,
-      }
-    }
-  },
-})*/
-
 // https://vitejs.dev/config/
 
 import { defineConfig } from 'vite';
